f5816150381574638837b39eb5b1129d3a1372d5
Ladly <vukivanovic.vuk@gmail.com>
2018-05-28 17:55:25 +0200

    T3 - Create landing page

--- app/src/containers/App/index.js
+++ app/src/containers/App/index.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react'
 
-import { AirTrafficService } from './../../../services/AirTrafficService'
-
-import { PlaneSvg } from './../../components/Svg'
-
 import './style.scss'
 
 class App extends Component {
+	
+	state = {
+		authorised: false
+	}
 
 	componentDidMount() {
-		AirTrafficService.getAllTrafic()
+		this.allowGeolocation()
+	}
+
+	allowGeolocation = () => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const pos = {}
+			pos.latitude = position.coords.latitude
+			pos.longitude = position.coords.longitude
+			const posJson = JSON.stringify(pos) 
+			localStorage.setItem('coordinates', posJson)
+			this.setState({authorised : true})
+		}, () => {
+			this.setState({authorised: false})
+		})
+	}
+
+	displayAuthorisationConversation = () => {
+		return this.state.authorised ? 
+			(
+				<div className="alert alert-info text-center">
+					<h6 className="landing-page-main">You grant me geolocation so now you can</h6>
+					<button className="btn btn-primary landing-page-main">Proceed</button>
+				</div>
+			) : 
+			(
+				<div>
+					<div className="alert alert-warning text-center">
+						<h6 className="landing-page-main">To use this site please allow geolocation</h6>
+					</div>
+				</div>
+			)
 	}
 
 	render (){
 		return (
-			<PlaneSvg />
-			// <h1>Yay</h1>
+			<div className="container">
+				<header className="jumbotron text-center">
+					<h1>Welcome To Air Traffic</h1>
+				</header>
+				{this.displayAuthorisationConversation()}
+			</div>
 		)
 	}
 }
